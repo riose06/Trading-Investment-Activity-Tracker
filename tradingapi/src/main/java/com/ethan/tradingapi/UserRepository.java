5e8b0d719eac84e78b55d75a97556d889d0c8a64
Ethan Rios <eirios2006@gmail.com>
2026-01-08 10:42:51 -0800

    Create and implement UserRepository.java

---
+++ tradingapi/src/main/java/com/ethan/tradingapi/UserRepository.java
@@ -0,0 +1,9 @@
+package main.java.com.ethan.tradingapi;
+
+import com.yourname.trackerapi.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
