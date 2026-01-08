749277636088046ad7b915a5321e2319ec75ac58
Ethan Rios <eirios2006@gmail.com>
2026-01-08 10:24:14 -0800

    Create and implement HealthController moduleas a health check endpoint

---
+++ tradingapi/src/main/java/com/ethan/tradingapi/HealthController.java
@@ -0,0 +1,13 @@
+package main.java.com.ethan.tradingapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "API is running";
+    }
+}
